f051635af7927f771306217041652e964904775e
cheap95 <cheap95@gmail.com>
2021-02-27 14:34:32 +0900

    ログアウト実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,12 @@ class UsersController < ApplicationController
     def edit
     end
     def update
-        current_user.update(user_params)
+        if current_user.update(user_params)
+            redirect_to root_path
+        else
+            render :edit
+        end
+
     end
         
       private
